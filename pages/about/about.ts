2a8a892bb6f79eb5b3a0d132e765540fdc389b26
shrinnivas <php.cnu@gmail.com>
2020-06-15 09:22:53 +0530

    2 files modified

--- pages/about/about.ts
+++ pages/about/about.ts
@@ -1,14 +1,26 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component } from "@angular/core";
+import { NavController } from "ionic-angular";
+import { Data } from "../home/home.service";
 
 @Component({
-  selector: 'page-about',
-  templateUrl: 'about.html'
+  selector: "page-about",
+  templateUrl: "about.html"
 })
 export class AboutPage {
+  searchTerm: any = "";
+  jsonData: any;
+  loanList: any;
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public data: Data) {}
 
+
+  ionViewDidLoad() {
+    this.setFilteredItems();
   }
 
+  setFilteredItems() {
+    this.jsonData = this.data.filterItems(this.searchTerm);
+    this.loanList = this.data.filterLoanItems(this.searchTerm);
+  }
+  
 }
